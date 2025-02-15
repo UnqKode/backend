395fafa32cd2dbfd90f9ab00983f230b7cf9204d
UnqKode <mycodeunq@gmail.com>
2025-02-15 20:21:42 +0530

    All routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,19 @@
-import {Router} from "express"
-import { registerUser , loginUser, logOutUser,refreshAccessToken} from "../controllers/user.controller.js"
+import { Router } from "express"
+import { registerUser, loginUser, logOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetail, updateUserAvatar, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
-import { verifyJWT} from "../middlewares/auth.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser
@@ -30,6 +30,37 @@ router.route("/logout").post(
 
 router.route("/refresh-token").post(
     refreshAccessToken
+)
+
+router.route("/change-password").post(
+    verifyJWT,
+    changeCurrentPassword
+)
+
+router.route("/current-user").get(
+    verifyJWT,
+    getCurrentUser
+)
+
+router.route("/update-account").patch(
+    verifyJWT,
+    updateAccountDetail
+)
+
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+)
+
+router.route("/C/:username").get(
+    verifyJWT,
+    getUserChannelProfile
+)
+
+router.route("/history").get(
+    verifyJWT,
+    getWatchHistory
 )
 
 export default router
